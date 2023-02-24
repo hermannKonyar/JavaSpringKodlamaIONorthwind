c9a9f32033293dbb32d7d519ff1ca4c0f672fb6c
erman konyar <er.konyar@gmail.com>
2023-02-24 12:36:48 +0300

    First Commit

---
+++ src/main/java/kodlamaio/nortwind/business/concretes/ProductManager.java
@@ -0,0 +1,29 @@
+package kodlamaio.nortwind.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.nortwind.business.abstracts.ProductService;
+import kodlamaio.nortwind.dataAccess.abstracts.ProductDao;
+import kodlamaio.nortwind.entities.concretes.Product;
+
+@Service
+public class ProductManager implements ProductService {
+
+	private ProductDao productDao;
+	
+	@Autowired
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		return this.productDao.findAll();
+	}
+
+}
